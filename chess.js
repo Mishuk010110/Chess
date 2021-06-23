e1bc75ae673fb5248131352165ef78f4b9d58113
Maksym <maksym.timofyeyev@gmail.com>
2021-06-23 22:11:14 +0300

    b,r,q

--- chess.js
+++ chess.js
@@ -4,9 +4,9 @@ var divFigure = '<div id="f%coord" class="figure">$figure</div>'
 $(function () {
     start()
     //$('.buttonNew').click(newFiguresPHP)
-       
-    
-    
+
+
+
 });
 
 function start() {
@@ -28,7 +28,7 @@ function getV(coord) {
 }
 
 function getH(coord) {
-    return 8-(coord - (coord % 8))/8;
+    return 8 - (coord - (coord % 8)) / 8;
 }
 
 function getCell(v, h) {
@@ -53,8 +53,8 @@ function canMove(z) {
     var frCoord = id.substring(1)
     var toCoord = this.id.substring(1)
     var fromInfo = {
-        figure : map[frCoord],
-        h : getH(frCoord),
+        figure: map[frCoord],
+        h: getH(frCoord),
         v: getV(frCoord),
         coord: frCoord
     }
@@ -65,13 +65,13 @@ function canMove(z) {
     var cell = map[toCoord];
     var cellIsEmpty = !cell || cell === "1";
     var toInfo = {
-        figure: cellIsEmpty?"1":cell,
+        figure: cellIsEmpty ? "1" : cell,
         h: getH(toCoord),
         v: getV(toCoord),
-         coord: toCoord,
+        coord: toCoord,
         isEmpty: cellIsEmpty
     }
-     console.log("from:")
+    console.log("from:")
     console.log(fromInfo)
     console.log("to:")
     console.log(toInfo)
@@ -83,6 +83,12 @@ function canMove(z) {
         case "n": return checkn(fromInfo, toInfo);
         case "K": return checkK(fromInfo, toInfo);
         case "k": return checkk(fromInfo, toInfo);
+        case "B": return checkB(fromInfo, toInfo);
+        case "b": return checkb(fromInfo, toInfo);
+        case "R": return checkR(fromInfo, toInfo);
+        case "r": return checkr(fromInfo, toInfo);
+        case "Q": return checkQ(fromInfo, toInfo);
+        case "q": return checkq(fromInfo, toInfo);
 
 
         default:
@@ -90,30 +96,30 @@ function canMove(z) {
 
     }
 
-    
+
 }
 
 
 function setDroppable() {
     $('.square').droppable(
         {
-        accept: canMove,
-        drop:function (event, ui) {
-            var frCoord = ui.draggable.attr('id').substring(1)
-            var toCoord = this.id.substring(1)
-            
-            moveFigure(frCoord, toCoord)
-        }
-    })
+            accept: canMove,
+            drop: function (event, ui) {
+                var frCoord = ui.draggable.attr('id').substring(1)
+                var toCoord = this.id.substring(1)
 
- }
+                moveFigure(frCoord, toCoord)
+            }
+        })
+
+}
 
 function moveFigure(frCoord, toCoord) {
     console.log('move from' + frCoord + 'to' + toCoord)
     figure = map[frCoord]
     showFigureAt(frCoord, '1')
     showFigureAt(toCoord, figure)
-      
+
 }
 
 
@@ -123,15 +129,15 @@ function addSquares() {
     console.log('addSquares')
     $('.board').html('');
     for (var coord = 0; coord < 64; coord++)
-    $('.board').append(divSquare.replace('%coord', coord).replace('%color', isBlackSquareAt(coord) ? 'black' : 'white'));
+        $('.board').append(divSquare.replace('%coord', coord).replace('%color', isBlackSquareAt(coord) ? 'black' : 'white'));
     setDroppable()
 }
 
 
-function showFigures(figures){
+function showFigures(figures) {
     for (var coord = 0; coord < 64; coord++)
         showFigureAt(coord, figures.charAt(coord))
-    
+
 }
 
 function showFigureAt(coord, figure) {
@@ -144,19 +150,19 @@ function showFigureAt(coord, figure) {
 
 function getChessSymbol(figure) {
     switch (figure) {
-        case 'K' : return '&#9812'
-        case 'Q' : return '&#9813'
-        case 'R' : return '&#9814'
-        case 'B' : return '&#9815'
-        case 'N' : return '&#9816'
-        case 'P' : return '&#9817'
-        case 'k' : return '&#9818'
-        case 'q' : return '&#9819'
-        case 'r' : return '&#9820'
-        case 'b' : return '&#9821'
-        case 'n' : return '&#9822'
-        case 'p' : return '&#9823'
-        default : return ''
+        case 'K': return '&#9812'
+        case 'Q': return '&#9813'
+        case 'R': return '&#9814'
+        case 'B': return '&#9815'
+        case 'N': return '&#9816'
+        case 'P': return '&#9817'
+        case 'k': return '&#9818'
+        case 'q': return '&#9819'
+        case 'r': return '&#9820'
+        case 'b': return '&#9821'
+        case 'n': return '&#9822'
+        case 'p': return '&#9823'
+        default: return ''
     }
 }
 
@@ -168,20 +174,31 @@ function newGame() {
     start();
 }
 
+function getInfo(v, h) {
+    var coord = getCell(v, h);
+    return {
+        figure: map[coord],
+        h: h,
+        v: v,
+        coord: coord
+    }
+
+}
 
 function checkP(f, t) {
-    
+
     if (t.h <= f.h) return false;
     if (t.h === f.h + 1 && (t.v === f.v + 1 || t.v === f.v - 1) && ("rnbqkp".includes(t.figure))) return true;
     if (t.v === f.v && (t.h === f.h + 1) && t.isEmpty) return true;
-    if (t.v === f.v && (t.h === f.h + 2) && f.h === 2 && t.isEmpty) return true;
+    if (t.v === f.v && (t.h === f.h + 2) && f.h === 2 && t.isEmpty && getInfo(f.v,3).figure === "1") return true;
+    
 }
 function checkp(f, t) {
 
     if (t.h >= f.h) return false;
     if (t.h === f.h - 1 && (t.v === f.v + 1 || t.v === f.v - 1) && ("RNBQKP".includes(t.figure))) return true;
     if (t.v === f.v && (t.h === f.h - 1) && t.isEmpty) return true;
-    if (t.v === f.v && (t.h === f.h - 2) && f.h === 7 && t.isEmpty) return true;
+    if (t.v === f.v && (t.h === f.h - 2) && f.h === 7 && t.isEmpty && getInfo(f.v, 6).figure === "1") return true;
 }
 function checkN(f, t) {
     if ("RNBQKP".includes(t.figure)) return false;
@@ -190,14 +207,14 @@ function checkN(f, t) {
     var dv = t.v - f.v;
 
 
-    if (dv===2 && dh===-1) return true;
-    if (dv===2 && dh===1) return true;
-    if (dv===-2 && dh===-1) return true;
-    if (dv===-2 && dh===1) return true;
-     if (dv===1 && dh===-2) return true;
-    if (dv===1 && dh===2) return true;
-    if (dv===-1 && dh===-2) return true;
-    if (dv===-1 && dh===2) return true;
+    if (dv === 2 && dh === -1) return true;
+    if (dv === 2 && dh === 1) return true;
+    if (dv === -2 && dh === -1) return true;
+    if (dv === -2 && dh === 1) return true;
+    if (dv === 1 && dh === -2) return true;
+    if (dv === 1 && dh === 2) return true;
+    if (dv === -1 && dh === -2) return true;
+    if (dv === -1 && dh === 2) return true;
 }
 function checkn(f, t) {
     if ("rnbqkp".includes(t.figure)) return false;
@@ -206,14 +223,14 @@ function checkn(f, t) {
     var dv = t.v - f.v;
 
 
-    if (dv===2 && dh===-1) return true;
-    if (dv===2 && dh===1) return true;
-    if (dv===-2 && dh===-1) return true;
-    if (dv===-2 && dh===1) return true;
-    if (dv===1 && dh===-2) return true;
-    if (dv===1 && dh===2) return true;
-    if (dv===-1 && dh===-2) return true;
-    if (dv===-1 && dh===2) return true;
+    if (dv === 2 && dh === -1) return true;
+    if (dv === 2 && dh === 1) return true;
+    if (dv === -2 && dh === -1) return true;
+    if (dv === -2 && dh === 1) return true;
+    if (dv === 1 && dh === -2) return true;
+    if (dv === 1 && dh === 2) return true;
+    if (dv === -1 && dh === -2) return true;
+    if (dv === -1 && dh === 2) return true;
 }
 function checkk(f, t) {
     if ("rnbqkp".includes(t.figure)) return false;
@@ -222,14 +239,14 @@ function checkk(f, t) {
     var dv = t.v - f.v;
 
 
-    if (dv===1 && dh===-1) return true;
-    if (dv===1 && dh===0) return true;
-    if (dv===1 && dh===1) return true;
-    if (dv===0 && dh===1) return true;
-    if (dv===0 && dh===-1) return true;
-    if (dv===-1 && dh===1) return true;
-    if (dv===-1 && dh===0) return true;
-    if (dv===-1 && dh===-1) return true;
+    if (dv === 1 && dh === -1) return true;
+    if (dv === 1 && dh === 0) return true;
+    if (dv === 1 && dh === 1) return true;
+    if (dv === 0 && dh === 1) return true;
+    if (dv === 0 && dh === -1) return true;
+    if (dv === -1 && dh === 1) return true;
+    if (dv === -1 && dh === 0) return true;
+    if (dv === -1 && dh === -1) return true;
 }
 function checkK(f, t) {
     if ("RNBQKP".includes(t.figure)) return false;
@@ -238,12 +255,95 @@ function checkK(f, t) {
     var dv = t.v - f.v;
 
 
-    if (dv===1 && dh===-1) return true;
-    if (dv===1 && dh===0) return true;
-    if (dv===1 && dh===1) return true;
-    if (dv===0 && dh===1) return true;
-    if (dv===0 && dh===-1) return true;
-    if (dv===-1 && dh===1) return true;
-    if (dv===-1 && dh===0) return true;
-    if (dv===-1 && dh===-1) return true;
+    if (dv === 1 && dh === -1) return true;
+    if (dv === 1 && dh === 0) return true;
+    if (dv === 1 && dh === 1) return true;
+    if (dv === 0 && dh === 1) return true;
+    if (dv === 0 && dh === -1) return true;
+    if (dv === -1 && dh === 1) return true;
+    if (dv === -1 && dh === 0) return true;
+    if (dv === -1 && dh === -1) return true;
 }
+
+
+
+
+function checkB(f, t) {
+    if ("RNBQKP".includes(t.figure)) return false;
+
+    var dh = t.h - f.h;
+    var dv = t.v - f.v;
+
+    if (Math.abs(dh) !== Math.abs(dv)) return false;
+    var hSign = (dh > 0) ? 1 : -1
+    var vSign = (dv > 0) ? 1 : -1;
+    for (var i = 1; i < Math.abs(dh); i++) {
+        var inf = getInfo(f.v + vSign * i, f.h + hSign * i);
+        if (inf.figure !== "1") return false;
+    }
+    return true;
+}
+function checkb(f, t) {
+    if ("rnbqkp".includes(t.figure)) return false;
+
+    var dh = t.h - f.h;
+    var dv = t.v - f.v;
+
+    if (Math.abs(dh) !== Math.abs(dv)) return false;
+    var hSign = (dh > 0) ? 1 : -1
+    var vSign = (dv > 0) ? 1 : -1;
+    for (var i = 1; i < Math.abs(dh); i++) {
+        var inf = getInfo(f.v + vSign * i, f.h + hSign * i);
+        if (inf.figure !== "1") return false;
+    }
+    return true;
+}
+function checkR(f, t) {
+    if ("RNBQKP".includes(t.figure)) return false;
+    if (!(t.h === f.h || t.v === f.v)) return false;
+
+    if (t.h === f.h) {
+        var dv = t.v - f.v;
+        var vSign = (dv > 0) ? 1 : -1;
+        for (var i = 1; i < Math.abs(dv); i++) {
+            var inf = getInfo(f.v + vSign * i, f.h);
+            if (inf.figure !== "1") return false;
+        }
+    } else {
+        var dh = t.h - f.h;
+        var hSign = (dh > 0) ? 1 : -1;
+        for (var i = 1; i < Math.abs(dh); i++) {
+            var inf = getInfo(f.v, f.h + hSign * i);
+            if (inf.figure !== "1") return false;
+        }
+    }
+    return true;
+}
+function checkr(f, t) {
+    if ("rnbqkp".includes(t.figure)) return false;
+    if (!(t.h === f.h || t.v === f.v)) return false;
+
+    if (t.h === f.h) {
+        var dv = t.v - f.v;
+        var vSign = (dv > 0) ? 1 : -1;
+        for (var i = 1; i < Math.abs(dv); i++) {
+            var inf = getInfo(f.v + vSign * i, f.h);
+            if (inf.figure !== "1") return false;
+        }
+    } else {
+        var dh = t.h - f.h;
+        var hSign = (dh > 0) ? 1 : -1;
+        for (var i = 1; i < Math.abs(dh); i++) {
+            var inf = getInfo(f.v, f.h + hSign * i);
+            if (inf.figure !== "1") return false;
+        }
+    }
+    return true;
+}
+function checkQ(f, t) {
+    return checkR(f, t) || checkB(f, t);
+}
+function checkq(f, t) {
+    return checkr(f, t) || checkb(f, t);
+}
+
